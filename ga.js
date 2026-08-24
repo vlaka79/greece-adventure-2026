6411b61d26138f16e1d33af76234776ffd78a68e
vlaka79 <daniel.boone79@gmail.com>
2026-08-24 15:23:49 -0700

    Add Google Analytics tag G-F3JZ07W6BG

---
+++ ga.js
@@ -0,0 +1,10 @@
+(function () {
+  var s = document.createElement("script");
+  s.async = true;
+  s.src = "https://www.googletagmanager.com/gtag/js?id=G-F3JZ07W6BG";
+  document.head.appendChild(s);
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = function () { window.dataLayer.push(arguments); };
+  window.gtag("js", new Date());
+  window.gtag("config", "G-F3JZ07W6BG");
+})();
